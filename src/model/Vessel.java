6a2e39928649e065779c07ce3db8d4d224666a31
Dhanavarshini <dp9387@srmist.edu.in>
2026-02-03 14:50:44 +0530

    [dhana]:UC1: Create Vessel model class
    - Defined Vessel entity
    - Added attributes, constructors, getters and setters
    - Followed JavaBean conventions

---
+++ src/model/Vessel.java
@@ -0,0 +1,61 @@
+package model;
+
+public class Vessel {
+
+    // unique vessel id
+    private String vesselId;
+
+    // vessel name
+    private String vesselName;
+
+    // speed in knots
+    private double averageSpeed;
+
+    // vessel category
+    private String vesselType;
+
+    // no-arg constructor
+    public Vessel() {
+    }
+
+    // constructor to initialize all fields
+    public Vessel(String vesselId, String vesselName, double averageSpeed, String vesselType) {
+        this.vesselId = vesselId;
+        this.vesselName = vesselName;
+        this.averageSpeed = averageSpeed;
+        this.vesselType = vesselType;
+    }
+
+    // getters and setters
+    public String getVesselId() {
+        return vesselId;
+    }
+
+    public void setVesselId(String vesselId) {
+        this.vesselId = vesselId;
+    }
+
+    public String getVesselName() {
+        return vesselName;
+    }
+
+    public void setVesselName(String vesselName) {
+        this.vesselName = vesselName;
+    }
+
+    public double getAverageSpeed() {
+        return averageSpeed;
+    }
+
+    public void setAverageSpeed(double averageSpeed) {
+        this.averageSpeed = averageSpeed;
+    }
+
+    public String getVesselType() {
+        return vesselType;
+    }
+
+    public void setVesselType(String vesselType) {
+        this.vesselType = vesselType;
+    }
+}

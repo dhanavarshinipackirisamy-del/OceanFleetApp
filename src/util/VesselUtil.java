fedeed9db6f89999afc261c3eb0ae55d0761b0a5
Dhanavarshini <dp9387@srmist.edu.in>
2026-02-03 15:27:13 +0530

    [dhana]: UC3 – Retrieve vessel by vessel id

--- src/util/VesselUtil.java
+++ src/util/VesselUtil.java
@@ -18,4 +18,19 @@ public class VesselUtil {
     public List<Vessel> getVesselList() {
         return vesselList;
     }
+    // returns vessel object for given vesselId
+    public Vessel getVesselById(String vesselId) {
+
+        // iterate through vessel list
+        for (Vessel vessel : vesselList) {
+
+            // case-sensitive comparison
+            if (vessel.getVesselId().equals(vesselId)) {
+                return vessel; // vessel found
+            }
+        }
+
+        // if no vessel matches
+        return null;
+    }
 }
